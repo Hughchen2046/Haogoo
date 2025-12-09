f8511a077e460e4898616855ca689eab9b653223
Hughchen2046 <hughchen2046@gmail.com>
2025-12-09 22:43:11 +0800

    環境部屬

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Haogoo
+# React + Vite
+
+目前已建立的專案先以Vite+React (JS+React-complier) + SCSS + Bootstrap + ECharts + Axios + gh-pages為優先,後續若有需要新增Router再修改.
+CSS請在assets/all.scss中進行設定
+JSX我先放在public中
+
+## 指令
+npm install - 安裝套件(初始導入請先安裝套件)
+npm run dev - 開發模式
+npm run build - 建置模式
+npm run preview - 預覽模式
+npm run deploy - 部署GH-PAGES模式
+
+## React Compiler
+
+The React Compiler is enabled on this template. See [this documentation](https://react.dev/learn/react-compiler) for more information.
+
+Note: This will impact Vite dev & build performances.
+
+## Expanding the ESLint configuration
+
+If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { useRef } from 'react'
+import axios from 'axios'
+import './App.css'
+import './assets/all.scss'
+import HaoGooGauge from '../public/Gauge.jsx'
+
+const {VITE_API_URL} = import.meta.env
+
+function App() {
+  const [count, setCount] = useState(0)
+  const modalRef = useRef(null)
+  const customModal = useRef(null)
+  useEffect(()=>{
+    (async () => {
+      const res = await axios.get(VITE_API_URL)
+      console.log(res);
+      openModal()
+      setTimeout(() => {
+        closeModal()
+      }, 2000);
+    })()
+  })
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className="mt-3">專案進度</h1>
+      <HaoGooGauge />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
