42173058198da2a2b315106b778dadd81428aa14
Hughchen2046 <hughchen2046@gmail.com>
2025-12-23 18:35:44 +0800

    更新資料加排序

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,13 @@ function App() {
     axios.get(VITE_stocksUrl)
     .then(response => {
       const filterData = response.data.filter((item)=> item.prices.length > 0)
-      const lastPrice = filterData.map((e)=>{
+      const lastData = filterData.map((e)=>{
         e.prices = e.prices.slice(-1)
         return e
+      }).sort((a, b)=> {
+        return b.industry.localeCompare(a.industry)
       })
-      setStockData(lastPrice)
+      setStockData(lastData)
       // console.log(lastPrice)
     })
     .catch(error => {
@@ -32,32 +34,35 @@ function App() {
           <table className="table table-hover table-striped mt-4">
             <thead>
               <tr>
-                <th>股票代碼</th>
-                <th>股票名稱</th>
-                <th>股票種類</th>
-                <th>產業類別</th>
-                <th>開盤價</th>
-                <th>收盤價</th>
-                <th>最高價</th>
-                <th>最低價</th>
-                <th>成交量</th>
-                <th>資料日期</th>
+                <th className='text-end'>股票代碼</th>
+                <th className='text-end'>股票名稱</th>
+                <th className='text-end'>股票種類</th>
+                <th className='text-end'>產業類別</th>
+                <th className='text-end'>開盤價</th>
+                <th className='text-end'>收盤價</th>
+                <th className='text-end'>最高價</th>
+                <th className='text-end'>最低價</th>
+                <th className='text-end'>成交量</th>
+                <th className='text-end'>漲跌%</th>
+                <th className='text-end'>資料日期</th>
               </tr>
             </thead>
             <tbody>
               {stockData.map((data)=>{
+                const dailyChangeRate = (data.prices[0].close - data.prices[0].open) / data.prices[0].open * 100
                 return(
                   <tr key={data.id}>
-                    <td>{data.id}</td>
-                    <td>{data.name}</td>
-                    <td>{data.SECURITY_TW}</td>
-                    <td>{data.industryTW}</td>
-                    <td>{data.prices[0].open.toFixed(2)}</td>
-                    <td>{data.prices[0].close.toFixed(2)}</td>
-                    <td>{data.prices[0].high.toFixed(2)}</td>
-                    <td>{data.prices[0].low.toFixed(2)}</td>
-                    <td>{data.prices[0].volume}</td>
-                    <td>{new Date(data.prices[0].date).toLocaleString().split(',')[0]}</td>
+                    <td className='text-end'>{data.id}</td>
+                    <td className='text-end'>{data.name}</td>
+                    <td className='text-end'>{data.SECURITY_TW}</td>
+                    <td className='text-end'>{data.industryTW}</td>
+                    <td className='text-end'>{data.prices[0].open.toFixed(2)}</td>
+                    <td className='text-end' style={{color: data.prices[0].close - data.prices[0].open > 0 ? 'red' : 'green'}}>{data.prices[0].close.toFixed(2)}</td>
+                    <td className='text-end'>{data.prices[0].high.toFixed(2)}</td>
+                    <td className='text-end'>{data.prices[0].low.toFixed(2)}</td>
+                    <td className='text-end'>{data.prices[0].volume}</td>
+                    <td className='text-end' style={{color: dailyChangeRate > 0 ? 'red' : 'green'}}>{dailyChangeRate.toFixed(2)}%</td>
+                    <td className='text-end'>{new Date(data.prices[0].date).toLocaleString().split(',')[0]}</td>
                   </tr>
                 )
               })}
