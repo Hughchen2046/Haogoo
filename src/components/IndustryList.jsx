253f3b74d37383ed2297ace60d4034205892a131
Hughchen2046 <hughchen2046@gmail.com>
2025-12-29 22:27:44 +0800

    更新jsx 草稿

---
+++ src/components/IndustryList.jsx
@@ -0,0 +1,12 @@
+export default function IndustryList() {
+  return (
+    <div className="industry-list">
+      <h2>Industry List</h2>
+      <ul>
+        <li>半導體</li>
+        <li>金融</li>
+        <li>生技醫療</li>
+      </ul>
+    </div>
+  )
+}
