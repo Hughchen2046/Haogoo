d6868e38ad0d3a29e9ba196af075ed4751c81c16
Hughchen2046 <hughchen2046@gmail.com>
2026-01-16 01:04:42 +0800

    調整

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,52 @@
+import { useState, useEffect } from 'react';
+import ButtonPrimary from './ButtonPrimary';
+import Logo from './Logo';
+import { Search } from 'lucide-react';
+
 export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 200);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-gray-600 py-0 ">
-        <div className="container ">
+      <nav
+        className={`navbar navbar-expand-md position-fixed top-0 start-0 end-0 p-12 font-zh-tw transition-all ${
+          isScrolled ? 'navbar-scrolled' : ''
+        }`}
+        style={{ zIndex: 999 }}
+        data-bs-theme="dark"
+      >
+        <div className="container d-flex gap-md-48 justify-content-between px-0 ">
           <a className="navbar-brand" href="#">
-            <img
-              src="\src\assets\WH-HaoGoo-Logo.png"
-              alt="Logo"
-              width="153px"
-              height="40px"
-              class="d-inline-block align-text-top"
-            />
+            <Logo className="nav-logo" />
           </a>
+          <ButtonPrimary className="w-auto py-10 px-24 d-md-none">免費註冊</ButtonPrimary>
           <button
-            className="navbar-toggler"
+            className="navbar-toggler "
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            style={{ '--bs-navbar-toggler-focus-width': '0.1rem' }}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            {/* 搜尋框 */}
-            <form className="position-relative" style={{ width: 599, height: 48 }}>
+            <form className="position-relative w-100" style={{ height: 48 }}>
               <input
                 type="search"
-                className="form-control h-100 pe-5 bg-gray-600 text-gray-800"
+                className="form-control h-100 pe-5 bg-transparent text-gray-200 placeholder-gray-200 border-gray-300"
                 placeholder="輸入台/美股代號，查看公司價值"
+                style={{ fontWeight: 400 }}
               />
 
               <button
@@ -37,31 +54,28 @@ export default function Navbar() {
                 className="position-absolute top-50 end-0 translate-middle-y me-3 border-0 bg-transparent p-0 d-flex align-items-center justify-content-center"
                 aria-label="search"
               >
-                <i className="bi bi-search fs-5 text-white"></i>
+                <Search size={24} />
               </button>
             </form>
           </div>
 
-          <ul className="navbar-nav my-20">
+          <ul className="navbar-nav d-none d-md-flex gap-md-8">
             <li className="nav-item">
-              <a className="nav-link text-gray-300 fw-bold tx-h6 mx-8" href="#">
+              <a className="nav-link text-gray-300 py-10 px-16" href="#">
                 我的選股清單
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-gray-300 fw-bold tx-h6 mx-8" href="#">
+              <a className="nav-link text-gray-300 py-10 px-16" href="#">
                 熱門話題
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-gray-300 fw-bold tx-h6 mx-8" href="#">
+              <a className="nav-link text-gray-300 py-10 px-16" href="#">
                 登入
               </a>
             </li>
-            <button className="btn bg-primary btn-login text-white fw-bold tx-h6 round-8 mx-8">
-              免費註冊
-            </button>
-            
+            <ButtonPrimary className="w-auto py-10 px-32 ">免費註冊</ButtonPrimary>
           </ul>
         </div>
       </nav>
