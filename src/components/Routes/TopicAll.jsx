3d617323b43b59379c158f955d9ed28f6e9d4159
Hughchen2046 <hughchen2046@gmail.com>
2026-02-01 16:18:11 +0800

    update

--- src/components/Routes/TopicAll.jsx
+++ src/components/Routes/TopicAll.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import pigbank from '../../assets/pigbank.png';
 import { MessageCircleMore } from 'lucide-react';
 
 export default function TopicAll() {
